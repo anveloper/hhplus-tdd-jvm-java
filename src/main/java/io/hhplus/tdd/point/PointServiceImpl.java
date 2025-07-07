2394a786376d243047132750ef11b7ab7c20a826
anveloper <hitedin@gmail.com>
2025-07-07 20:10:38 +0900

    feat: 포인트 사용 기능 코드 작성 (GREEN -> REFACTOR)
    - 요구사항 2: 포인트 사용 함구 구현
      - 잔고가 부족할 경우 포인트 사용 실패

--- src/main/java/io/hhplus/tdd/point/PointServiceImpl.java
+++ src/main/java/io/hhplus/tdd/point/PointServiceImpl.java
@@ -21,14 +21,25 @@ public class PointServiceImpl implements PointService {
     @Override
     public UserPoint charge(long userId, long amount) {
         long prevPoint = userPointTable.selectById(userId).point(); // 아이디로 이전 금액을 조회하고,
+
         UserPoint updated = userPointTable.insertOrUpdate(userId, prevPoint + amount); // 포인트를 업데이트 한다.
         pointHistoryTable.insert(userId, amount, TransactionType.CHARGE, updated.updateMillis()); // 히스토리에 기록
+
         return updated; // 업데이트 된 포인트를 반환한다.
     }
 
     @Override
     public UserPoint use(long userId, long amount) {
-        return null;
+        long currentPoint = userPointTable.selectById(userId).point(); // 아이디로 현재 금액을 조회하고,
+
+        if (currentPoint < amount) { // 사용 금액과 비교하여, 부족하면 에러 처리
+            throw new IllegalStateException("포인트가 부족합니다."); //
+        }
+
+        UserPoint updated = userPointTable.insertOrUpdate(userId, currentPoint - amount); // 포인트를 업데이트
+        pointHistoryTable.insert(userId, amount, TransactionType.USE, updated.updateMillis()); // 히스토리 기록
+
+        return updated; // 업데이트 된 포인트 반환
     }
 
     @Override
