d112070d5880dca4e25f2770e6ffaff3a426ef02
anveloper <hitedin@gmail.com>
2025-07-08 23:51:55 +0900

    feat: 포인트 히스토리 조회 Controller 구현(GREEN)
    - service.getHistory 연결 완료

--- src/main/java/io/hhplus/tdd/point/PointController.java
+++ src/main/java/io/hhplus/tdd/point/PointController.java
@@ -28,13 +28,14 @@ public class PointController {
     }
 
     /**
-     * TODO - 특정 유저의 포인트 충전/이용 내역을 조회하는 기능을 작성해주세요.
+     * RESOLVED: 특정 유저의 포인트 충전/이용 내역을 조회하는 기능을 작성해주세요.
      */
     @GetMapping("{id}/histories")
     public List<PointHistory> history(
             @PathVariable long id
     ) {
-        return List.of();
+        log.info("GET /point/{}/histories 요청 수신", id);
+        return pointService.getHistory(id);
     }
 
     /**
